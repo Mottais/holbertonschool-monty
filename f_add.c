a419332c1341342e19d69ff0e74c09105d4e68d2
Mottais <mottais.afy@outlook.fr>
2024-01-05 02:48:29 -0800

    Correction commentaire dans f_add.c et ajout list_head->prev = NULL

--- f_add.c
+++ f_add.c
@@ -15,12 +15,15 @@ void f_add(stack_t **list_head, unsigned int num_ligne)
 		erreur = EXIT_FAILURE;
 		return;
 	}
-	/* ajoute la valeur du 2ieme élément au premier */
+	/* ajoute la valeur du 1er élément au 2ieme */
 	(*list_head)->next->n += (*list_head)->n;
 
-	/* réaffect l'addresse du 2ieme élément au 1er */
+	/* le 2ieme element devient la tête de pile */
 	(*list_head) = (*list_head)->next;
 
-	/* supp le 2ieme éléent de la pile */
+	/* prev du head = NULL */
+	(*list_head)->prev = NULL;
+
+	/* supp le 1er élément de la pile */
 	free((*list_head)->prev);
 }
