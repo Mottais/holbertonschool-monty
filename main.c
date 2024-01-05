fdc66a0098288c1236dc18119ee785b59c3f6160
Mottais <mottais.afy@outlook.fr>
2024-01-05 03:57:48 -0800

    Ajout de la fonction sub

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int arg_c, char **arg_v)
 	instruction_t tab_opcode_fonct[] = {
 		{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
 		{"pop", f_pop}, {"swap", f_swap}, {"add", f_add},
-		{"nop", f_nop}, {NULL, NULL}};
+		{"sub", f_sub}, {"nop", f_nop}, {NULL, NULL}};
 
 	if (arg_c != 2)
 	fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
